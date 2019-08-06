580ef67697ce33d5aef331301621964e05d8948e
Md-shefat-masum <myphoto240@gmail.com>
2019-08-06 17:18:30 +0600

    --- Auto-Git Commit ---

--- answer3/add.php
+++ answer3/add.php
@@ -1,3 +1,26 @@
+<?php 
+    $con=mysqli_connect('localhost','root','','student_infor');
+    if(!empty($_POST)){
+        $name=$_POST['name'];
+        $roll=$_POST['roll'];
+        $department=$_POST['department'];
+        $first=$_POST['first'];
+        $second=$_POST['second'];
+        $third=$_POST['third'];
+        $fourth=$_POST['fourth'];
+        $fifth=$_POST['fifth'];
+        $sixth=$_POST['sixth'];
+        $seventh=$_POST['seventh'];
+        $eighth=$_POST['eighth'];
+        $insert="INSERT INTO student_infor(name,roll,department,first,second,third,fourth,fifth,sixth,seventh,eighth)
+                            VALUES('$name','$roll','$department','$first','$third','$fourth','$fifth','$sixth','$seventh','$eighth')";
+
+        if(mysqli_query($con,$insert)){
+            echo "succecc";
+        }
+    }
+    
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,12 +51,25 @@
     <section id="login">
         <div class="container">
             <div class="login">
-                <form action="" style="" class="">
-                    <h2>Contact Us</h2>
-                    <input type="text" placeholder="name">
-                    <input placeholder="email" type="text">
-                    <textarea placeholder="write your feed back" id="" cols="30" rows="10"></textarea>
-                    <button style="width: 200px;">Send Message</button>
+                <form action="add.php" method="POST" style="" class="">
+                    <h2>Student Result Information</h2>
+                    <input name="name" type="text" placeholder="name">
+                    <input name="roll" type="text" placeholder="Roll">
+                    <input name="department" type="text" placeholder="Department">
+                    <h3>CGPA</h3>
+                    <div style="width:45%; float:left;margin: 0px 5px; overflow:hidden;">
+                        <input name="first" placeholder="1st Semester" type="number">
+                        <input name="second" placeholder="2nd Semester" type="number">
+                        <input name="third" placeholder="3rd Semester" type="number">
+                        <input name="fourth" placeholder="4th Semester" type="number">
+                    </div>
+                    <div style="width:50%; float:left;margin: 0px 5px;overflow:hidden;">
+                        <input name="fifth" placeholder="5th Semester" type="number">
+                        <input name="sixth" placeholder="6th Semester" type="number">
+                        <input name="seventh" placeholder="7th Semester" type="number">
+                        <input name="eighth" placeholder="8th Semester" type="number">
+                    </div>
+                    <button type="submit" style="width: 200px;">Add Information</button>
                 </form>
             </div>
         </div>
