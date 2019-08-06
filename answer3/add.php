727ecc9a084aaee8cd033604e397f7e7a2abb189
Md-shefat-masum <myphoto240@gmail.com>
2019-08-06 16:48:18 +0600

    --- Auto-Git Commit ---

---
+++ answer3/add.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Student's CGPA</title>
+</head>
+<body>
+    <section id="nav">
+        <div class="container">
+            <div class="nav">
+                <div class="left">
+                    <!--                    <img src="" alt="LOGO">-->
+                    <h2 style="color:white;">logo</h2>
+                </div>
+                <div class="right">
+                    <ul>
+                        <li><a href="index.php">HOME</a></li>
+                        <li><a href="add.php">ADD NEW INFO</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="login">
+        <div class="container">
+            <div class="login">
+                <form action="" style="" class="">
+                    <h2>Contact Us</h2>
+                    <input type="text" placeholder="name">
+                    <input placeholder="email" type="text">
+                    <textarea placeholder="write your feed back" id="" cols="30" rows="10"></textarea>
+                    <button style="width: 200px;">Send Message</button>
+                </form>
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
---
+++ answer3/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Student's CGPA</title>
+</head>
+<body>
+    <section id="nav">
+        <div class="container">
+            <div class="nav">
+                <div class="left">
+                    <!--                    <img src="" alt="LOGO">-->
+                    <h2 style="color:white;">logo</h2>
+                </div>
+                <div class="right">
+                    <ul>
+                        <li><a href="index.php">HOME</a></li>
+                        <li><a href="add.php">ADD NEW INFO</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <table class="table product-overview">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Roll</th>
+                        <th>Department</th>
+                        <th>GPA</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tr>
+                    <td colspan="6">
+                        <hr>
+                    </td>
+                </tr>
+                <tbody>
+                    <tr>
+                        <td>124323</td>
+                        <td>
+                            Habib
+                        </td>
+                        <td>18</td>
+                        <td>CSE</td>
+                        <td>
+                            4.00
+                        </td>
+                        <td>
+                            +
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>124323</td>
+                        <td>
+                            Habib
+                        </td>
+                        <td>18</td>
+                        <td>CSE</td>
+                        <td>
+                            4.00
+                        </td>
+                        <td>
+                            +
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>124323</td>
+                        <td>
+                            Habib
+                        </td>
+                        <td>18</td>
+                        <td>CSE</td>
+                        <td>
+                            4.00
+                        </td>
+                        <td>
+                            +
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
+
+</body>
+</html>
