89ef14f0a6139e2d6edfe4ef972d44444792e56b
Md-shefat-masum <myphoto240@gmail.com>
2019-08-27 09:50:17 +0600

    --- Auto-Git Commit ---

--- answer3/index2.php
+++ answer3/index2.php
@@ -1,3 +1,23 @@
+<?php 
+    $con=mysqli_connect('localhost','root','','student_infor');
+    if(!$con){
+        echo "<script> alert('db not success'); </script>";
+    }
+    
+    if(!empty($_POST)){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $department = $_POST['department'];
+        $gender = $_POST['gender'];
+        $role1 = $_POST['role1'];
+        $role2 = $_POST['role2'];
+        $role3 = $_POST['role3'];
+        $insert = "INSERT INTO student_information(name,email,password,department,gender,role1,role2,role3)
+                                    values($name,$email,$password,$department,$gender,$role1,$role2,$role3
+                                        )";
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,26 +33,26 @@
             <a href="addnew.php">add new record</a>
             <a href="addnew.php">view record</a>
         </div>
-        <form action="">
+        <form action="index2.php" method="POST">
             <label for="">your name</label>
-            <input type="text">
+            <input name="name" type="text">
             <label for="">your email</label>
-            <input type="text">
+            <input name="email" type="text">
             <label for="">your password</label>
-            <input type="password">
+            <input name="password" type="password">
             <label for="">department</label>
-            <select name="" id="">
+            <select name="department" id="">
                 <option value="cse">cse</option>
                 <option value="ece">ece</option>
                 <option value="bba">bba</option>
             </select>
             <label for="">gender</label>
-            <input type="checkbox"><span>male</span>
-            <input type="checkbox"><span>female</span>
+            <input type="checkbox" name="gender" value="male"><span>male</span>
+            <input type="checkbox" name="gender" value="female"><span>female</span>
             <label for="">your role</label>
-            <input type="radio" name=""><span>student</span>
-            <input type="radio" name=""><span>class captain</span>
-            <input type="radio" name=""><span>trainer</span><br>
+            <input type="radio" name="student" value="student"><span>student</span>
+            <input type="radio" name="captain" value="captain"><span>class captain</span>
+            <input type="radio" name="trainer" value="trainer"><span>trainer</span><br>
             <button type="submit">submit</button>
             <button type="reset">reset</button>
         </form>
