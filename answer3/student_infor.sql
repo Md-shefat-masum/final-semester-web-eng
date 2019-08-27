19e7e330cce16f37db30768e5e13e8e2ec9c0e30
Md-shefat-masum <myphoto240@gmail.com>
2019-08-27 23:38:59 +0600

    sql add

--- answer3/student_infor.sql
+++ answer3/student_infor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 06, 2019 at 02:14 PM
+-- Generation Time: Aug 27, 2019 at 07:38 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.12
 
@@ -51,6 +51,33 @@ INSERT INTO `gpa` (`id`, `roll`, `department`, `first`, `second`, `third`, `four
 (5, '21', 'cse', 4, 4, 4, 4, 4, 3, 3, 3, 'shefat'),
 (6, '21', 'cse', 2, 4, 2, 3, 1, 2, 3, 4, 'yasir');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_information`
+--
+
+CREATE TABLE `student_information` (
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `department` varchar(100) DEFAULT NULL,
+  `gender` varchar(100) DEFAULT NULL,
+  `role1` varchar(100) DEFAULT NULL,
+  `role2` varchar(100) DEFAULT NULL,
+  `role3` varchar(100) DEFAULT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student_information`
+--
+
+INSERT INTO `student_information` (`name`, `email`, `password`, `department`, `gender`, `role1`, `role2`, `role3`, `id`) VALUES
+('shefat', 'shefat@gmail.com', '123456', 'cse', 'male', 'student', 'captain', 'trainer', 11),
+('yasir', 'intu@gmail.com', '123456', 'ece', 'male', 'student', 'captain', '', 12),
+('nahiyan', 'nahiyan@gmail.com', '123456', 'bba', 'male', '', 'captain', 'trainer', 13);
+
 --
 -- Indexes for dumped tables
 --
@@ -62,6 +89,12 @@ ALTER TABLE `gpa`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `student_information`
+--
+ALTER TABLE `student_information`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -70,6 +103,12 @@ ALTER TABLE `gpa`
 --
 ALTER TABLE `gpa`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `student_information`
+--
+ALTER TABLE `student_information`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
